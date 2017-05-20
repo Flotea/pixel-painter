9b84794ab6245772c682b2721339a2ade8e0fb9f
Flotea <hgrimmer0786@myhta.org>
2017-05-19 15:01:30 -1000

    Wip3

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 function changeColorOnClick(idName){
-	alert(idName);
-	var originalElementColor = document.getElementById('selectedColorName'); 
-	document.getElementById(idName).style.backgroundcolor = originalElementColor;
-
+	var originalElementColor = document.getElementById('selectedColorName').style.backgroundColor; 
+	document.getElementById(idName).style.backgroundColor = originalElementColor;
 }
 function getBackgroundColor(idName){
-	var element = document .getElementById(idName);
+	var element = document.getElementById(idName);
 	var style = window.getComputedStyle(element,null).getPropertyValue("background-color");
-	document.getElementById('selectedColorName').style.backgroundcolor=style;
-	
-
+	document.getElementById('selectedColorName').style.backgroundColor=style;
 }
